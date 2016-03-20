c5b15830cc9bd505aa741252c31b8ca2e4228f63
Jeremy Bellows <Jeremy.Bellows@match.com>
2016-03-20 11:31:23 -0500

    Kinsect Scraper - Added all kinsect path parsing and fixed html layout to be compatible with type provider

--- table_exporter/kinsectScraper/kinsectScaper.fsx
+++ table_exporter/kinsectScraper/kinsectScaper.fsx
@@ -29,11 +29,11 @@ type RequiredStats =
     Speed : int
   }
 
-let convertStringsToRequiredStats (power : string) (stamina : string) (speed : string) =
+let convertIntsToRequiredStats power stamina speed =
   {
-    Power = int power
-    Stamina = int stamina
-    Speed = int speed
+    Power = power
+    Stamina = stamina
+    Speed = speed
   }
 
 type KinsectData =
@@ -48,9 +48,10 @@ type KinsectData =
 //I had to remove the cascading table headers and seperate them into
 //multiple tables so the type provider can work properly
 //Table1 : Blunt Path
-//Table2 : Event Quest Only
-//Table3 : Japanese Event Quest Only
+//Table2 : Blunt Path Event Quest Only
+//Table3 : Blunt Path Japanese Event Quest Only
 //Table4 : Cutting Path
+//Table5 : Cutting Path Japanese Event Quest Only
 //I also removed the required stats column and replaced it with headers power, stamina, speed
 //removed rowspan from headers because it messed up the type provider
 
@@ -60,12 +61,52 @@ type KinsectHtmlData = HtmlProvider<"./kinsectTable.html">
 let kinsectHtmlData = KinsectHtmlData.Load("./kinsectTable.html")
 kinsectHtmlData.Tables.Table1.Rows.[0]
 
-let table1Data =
+let bluntPathData =
   kinsectHtmlData.Tables.Table1.Rows
   |> Array.map(fun row ->
      {
        Name = row.Name
        Type = convertStringToKinsectType row.Type
        Skill = row.Skill
-       RequiredStats = convertStringsToRequiredStats row.Power row.Stamina row.Speed
+       RequiredStats = convertIntsToRequiredStats row.Power row.Stamina row.Speed
+     })
+
+let bluntPathEventQuestData =
+  kinsectHtmlData.Tables.Table2.Rows
+  |> Array.map(fun row ->
+     {
+       Name = row.Name
+       Type = convertStringToKinsectType row.Type
+       Skill = row.Skill
+       RequiredStats = convertIntsToRequiredStats row.Power row.Stamina row.Speed
+     })
+
+let bluntPathJapaneseEventData =
+  kinsectHtmlData.Tables.Table3.Rows
+  |> Array.map(fun row ->
+     {
+       Name = row.Name
+       Type = convertStringToKinsectType row.Type
+       Skill = row.Skill
+       RequiredStats = convertIntsToRequiredStats row.Power row.Stamina row.Speed
+     })
+
+let cuttingPathData =
+  kinsectHtmlData.Tables.Table4.Rows
+  |> Array.map(fun row ->
+     {
+       Name = row.Name
+       Type = convertStringToKinsectType row.Type
+       Skill = row.Skill
+       RequiredStats = convertIntsToRequiredStats row.Power row.Stamina row.Speed
+     })
+
+let cuttingPathJapaneseEventData =
+  kinsectHtmlData.Tables.Table5.Rows
+  |> Array.map(fun row ->
+     {
+       Name = row.Name
+       Type = convertStringToKinsectType row.Type
+       Skill = row.Skill
+       RequiredStats = convertIntsToRequiredStats row.Power row.Stamina row.Speed
      })
